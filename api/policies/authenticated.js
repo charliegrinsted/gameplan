933d9a2de699efcde73f2be3b92968182f427f46
Charlie <charlesgrinsted@gmail.com>
2015-01-29 10:57:56 +0000

    refresh the user each time to update changed info

--- api/policies/authenticated.js
+++ api/policies/authenticated.js
@@ -12,7 +12,14 @@ module.exports = function(req, res, next) {
   // User is allowed, proceed to the next policy, 
   // or if this is the last policy, the controller
 	if (req.session.authenticated) {
-		return next();
+		var currentUser = req.session.User.id;
+
+		User.findOneById(currentUser)
+		.populateAll()
+		.exec(function foundUser(err, user) {
+			req.session.User = user;
+			return next();
+		});
 	}
 
 	else {
